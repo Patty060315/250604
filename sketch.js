07aa68008805bc87e2d8c57da031694f9b4ceff4
Patty060315 <413730176@o365.tku.edu.tw>
2025-06-04 19:04:17 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -46,28 +46,35 @@ function setup() {
 
 function draw() {
   background(220);
+
+  // 顯示標題
+  textAlign(CENTER, TOP);
+  textSize(32);
+  fill(50, 50, 150);
+  noStroke();
+  text("淡江教育科技系", width / 2, 10);
+
   Engine.update(engine);
   strokeWeight(2);
   stroke(0);
-  
+
   // Draw the webcam video
   image(video, 0, 0, width, height);
-  
+
   if (random() < 0.1) {
     circles.push(new Circle());
   }
-  
+
   for (let i=circles.length-1; i>=0; i--) {
     circles[i].checkDone();
     circles[i].display();
-    
+
     if (circles[i].done) {
       circles[i].removeCircle();
       circles.splice(i, 1);
     }
-    
   }
-  
+
   if (hands.length > 0) {
     let thumb = hands[0].keypoints[THUMB_TIP];
     let index = hands[0].keypoints[INDEX_FINGER_TIP];
@@ -75,15 +82,13 @@ function draw() {
     noStroke();
     circle(thumb.x, thumb.y, 10);
     circle(index.x, index.y, 10);
-    
+
     bridge.bodies[0].position.x = thumb.x;
     bridge.bodies[0].position.y = thumb.y;
     bridge.bodies[bridge.bodies.length-1].position.x = index.x;
     bridge.bodies[bridge.bodies.length-1].position.y = index.y;
     bridge.display();
   }
-  
-  
 }
 
 // Callback function for when handPose outputs data
