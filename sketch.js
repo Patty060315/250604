02d18aa076b2d0fffbe6ca8a3534182a04733117
Patty060315 <413730176@o365.tku.edu.tw>
2025-06-04 20:08:15 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -22,7 +22,7 @@ let engine;
 let bridge; let num = 10; let radius = 10; let length = 25;
 let circles = [];
 
-let colorPalette = ["#edafb8", "#f7e1d7", "#dedbd2", "#b0c4b1", "#8d99ae", "#ef562f", "#598392", "#e0afa0"]; 
+let colorPalette = ["#f7e1d7", "#9d8189", "#8d99ae", "#62b6de", "#f589a3", "#81b29a", "#738290", "#cb997e"]; 
 
 function preload() {
   // Load the handPose model
@@ -50,32 +50,31 @@ function draw() {
   Engine.update(engine);
   strokeWeight(2);
   stroke(0);
-  
-  // Draw the webcam video
+
+  // 先畫攝影機畫面
   image(video, 0, 0, width, height);
 
-  // 顯示標題（移到這裡）
+  // 再顯示標題，才不會被蓋掉
   textAlign(CENTER, TOP);
   textSize(32);
-  fill(50, 50, 120);
+  fill(50, 50, 150);
   noStroke();
-  text("淡江教育科技系", width / 2, 20);
+  text("淡江教育科技系", width / 2, 10);
 
   if (random() < 0.1) {
     circles.push(new Circle());
   }
-  
+
   for (let i=circles.length-1; i>=0; i--) {
     circles[i].checkDone();
     circles[i].display();
-    
+
     if (circles[i].done) {
       circles[i].removeCircle();
       circles.splice(i, 1);
     }
-    
   }
-  
+
   if (hands.length > 0) {
     let thumb = hands[0].keypoints[THUMB_TIP];
     let index = hands[0].keypoints[INDEX_FINGER_TIP];
@@ -83,15 +82,13 @@ function draw() {
     noStroke();
     circle(thumb.x, thumb.y, 10);
     circle(index.x, index.y, 10);
-    
+
     bridge.bodies[0].position.x = thumb.x;
     bridge.bodies[0].position.y = thumb.y;
     bridge.bodies[bridge.bodies.length-1].position.x = index.x;
     bridge.bodies[bridge.bodies.length-1].position.y = index.y;
     bridge.display();
   }
-  
-  
 }
 
 // Callback function for when handPose outputs data
